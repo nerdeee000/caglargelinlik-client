113eb069797277694259f7f5fd12ee271738ec24
Reşat YAVÇİN <resatyavcin@Resat-MacBook-Pro.local>
2021-06-02 21:43:39 +0300

    navbar banner components and alert utils created

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import './App.css';
+import Navbar from './components/Navbar'
+import Banner from './components/Banner'
+import Alert from './utils/Alert'
+
+function App() {
+  return (
+    <div>
+            <Banner/>
+
+      <Navbar/> 
+
+
+        <div class="bg-gray-50">
+        <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
+          <h2 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+            <span class="block">Hazır mısınız?</span>
+            <span class="block text-indigo-600">Mutluluğunuza mutluluk katmaya...</span>
+          </h2>
+          <div class="mt-8 flex lg:mt-0 lg:flex-shrink-0">
+            <div class="inline-flex rounded-md shadow">
+              <a href="#" class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700">
+                Ürünlere Gözat
+              </a>
+            </div>
+            <div class="ml-3 inline-flex rounded-md shadow">
+              <a href="#" class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-indigo-50">
+                Daha Fazlası
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
